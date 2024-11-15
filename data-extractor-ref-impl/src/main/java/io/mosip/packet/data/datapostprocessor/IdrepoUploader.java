35142697094a2d0eb1c666e9edc745b37780d770
Muralitharan K <muralitharan@mosip.io>
2024-11-15 11:50:09 +0530

    Changes for revoke functionality

---
+++ data-extractor-ref-impl/src/main/java/io/mosip/packet/data/datapostprocessor/IdrepoUploader.java
@@ -0,0 +1,238 @@
+package io.mosip.packet.data.datapostprocessor;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import io.mosip.commons.packet.dto.packet.PacketDto;
+import io.mosip.kernel.biometrics.entities.BIR;
+import io.mosip.kernel.biometrics.spi.CbeffUtil;
+import io.mosip.kernel.core.logger.spi.Logger;
+import io.mosip.kernel.core.util.CryptoUtil;
+import io.mosip.kernel.core.util.DateUtils;
+import io.mosip.kernel.core.util.HMACUtils2;
+import io.mosip.packet.core.constant.tracker.TrackerStatus;
+import io.mosip.packet.core.dto.DataPostProcessorResponseDto;
+import io.mosip.packet.core.dto.DataProcessorResponseDto;
+import io.mosip.packet.core.dto.ResponseWrapper;
+import io.mosip.packet.core.logger.DataProcessLogger;
+import io.mosip.packet.core.service.thread.ResultDto;
+import io.mosip.packet.core.service.thread.ResultSetter;
+import io.mosip.packet.core.spi.datapostprocessor.DataPostProcessor;
+import io.mosip.packet.core.util.TrackerUtil;
+import io.mosip.packet.data.datapostprocessor.custom.ImportIdentityService;
+import io.mosip.packet.data.dto.DemographicDedupe;
+import io.mosip.packet.data.dto.Documents;
+import io.mosip.packet.data.dto.IdRequestDto;
+import io.mosip.packet.data.dto.RequestDto;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+import static io.mosip.packet.core.constant.RegistrationConstants.*;
+
+@Component
+public class IdrepoUploader implements DataPostProcessor {
+
+    private static final Logger logger = DataProcessLogger.getLogger(IdrepoUploader.class);
+    private static final String VERSION = "v1";
+    //private static final SimpleDateFormat simpleDateParser = new SimpleDateFormat("yyyy-MM-dd");
+    //private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @Autowired
+    private CbeffUtil cbeffUtil;
+
+    @Value("${mosip.kernel.xsdfile}")
+    private String cbeffXsd;
+
+    @Autowired
+    private ImportIdentityService importIdentityService;
+
+    @Autowired
+    private TrackerUtil trackerUtil;
+
+    private byte[] xsd;
+
+    @Override
+    public DataPostProcessorResponseDto postProcess(DataProcessorResponseDto processObject, ResultSetter setter, Long startTime) throws Exception {
+        DataPostProcessorResponseDto responseDto = new DataPostProcessorResponseDto();
+        responseDto.setProcess(processObject.getProcess());
+        responseDto.setRefId(processObject.getRefId());
+        responseDto.setTrackerRefId(processObject.getTrackerRefId());
+        responseDto.setResponses(new HashMap<>());
+
+        PacketDto packetDto = (PacketDto) processObject.getResponses().get("packetDto");
+        HashMap<String, Object> demoDetails = (HashMap<String, Object>) processObject.getResponses().get("demoDetails");
+        String trackerRefId = processObject.getTrackerRefId();
+
+        logger.info("Entering Idrepo identity Uploader, RID:{}, NRCID:{} ", packetDto.getId(),
+                packetDto.getFields().get("nrcId"));
+        Long timeDifference = System.nanoTime()-startTime;
+        logger.debug("SESSION_ID", APPLICATION_NAME, APPLICATION_ID, "Time Taken to enter the id repo file. " + trackerRefId + " " + TimeUnit.SECONDS.convert(timeDifference, TimeUnit.NANOSECONDS));
+
+        Map<String, Object> identity = new HashMap<>();
+        Map<String, String> packetFields = (Map<String, String>) packetDto.getFields();
+        for (Map.Entry<String, String> entry : packetFields.entrySet()) {
+            if (entry.getKey().equalsIgnoreCase("dateOfBirth")) {
+                //Date dob = simpleDateParser.parse(entry.getValue());
+                identity.put(entry.getKey(), entry.getValue().replaceAll("-","/"));
+            } else if (entry.getKey().equalsIgnoreCase("IDSchemaVersion")) {
+                identity.put(entry.getKey(), NumberUtils.createDouble(entry.getValue()));
+            } else if (entry.getKey().equalsIgnoreCase("phoneNumber")) {
+                if (StringUtils.hasText(entry.getValue())) {
+                    identity.put(entry.getKey(), entry.getValue().replaceAll("[\\s+?�^]*", ""));
+                }
+            } else if (entry.getKey().equalsIgnoreCase("nrcId")
+                    || entry.getKey().equalsIgnoreCase("UIN")
+                    || entry.getKey().equalsIgnoreCase("registrationId")
+                    || entry.getKey().equalsIgnoreCase("email")) {
+                if (StringUtils.hasText(entry.getValue())) {
+                    identity.put(entry.getKey(), entry.getValue());
+                }
+            } else if (entry.getKey().equalsIgnoreCase("addressLine")) {
+                if (StringUtils.hasText(entry.getValue())) {
+                    identity.put(entry.getKey(), mapper.readValue(entry.getValue(), Object.class));
+                }
+            } else if (entry.getKey().equalsIgnoreCase("selectedHandles")) {
+                identity.put(entry.getKey(), Arrays.asList(entry.getValue().split(",")));
+            } else {
+                identity.put(entry.getKey(), mapper.readValue(entry.getValue(), Object.class));
+            }
+        }
+
+        IdRequestDto idRequestDTO = new IdRequestDto();
+// Setting the identity JSON object to the requestDto
+        RequestDto requestDto = new RequestDto();
+        requestDto.setRegistrationId(packetDto.getId());
+        requestDto.setIdentity(identity);
+
+        //logger.info("Processor: RequestDto ( {} )", requestDto);
+        requestDto.setDocuments(getBiometricsAndDocuments(packetDto));
+        // Setting the requestDto to the idRequestDto
+        idRequestDTO.setId("mosip.id.create");
+        idRequestDTO.setVersion(VERSION);
+        idRequestDTO.setRequesttime(DateUtils.getUTCTimeFromDate(new Date()));
+        idRequestDTO.setRequest(requestDto);
+        ResponseWrapper response = importIdentityService.importIdentity(idRequestDTO, demoDetails);
+        if (response != null && response.getResponse() != null) {
+            logger.info("Import identity success, response: {}", response.getResponse());
+            responseDto.getResponses().put("message", "Import identity success, response: " + response.getResponse());
+            Map<String, Object> additionalMaps = prepareAdditionalMaps(demoDetails, packetDto);
+            trackerStatusUpdate(processObject.getRefId(), packetDto, setter, TrackerStatus.PROCESSED,  (new Gson()).toJson(responseDto), additionalMaps);
+        } else if (response != null && response.getErrors() != null) {
+            logger.error("Error response received: {}", response.getErrors());
+            responseDto.getResponses().put("message", "Error response received: "+ response.getErrors());
+            Map<String, Object> additionalMaps = Map.of("REF_CODE", demoDetails.get("SUBFOLDER"));
+            trackerStatusUpdate(processObject.getRefId(), packetDto, setter, TrackerStatus.FAILED, (new Gson()).toJson(responseDto), additionalMaps);
+        }
+        timeDifference = System.nanoTime()-startTime;
+        logger.debug("SESSION_ID", APPLICATION_NAME, APPLICATION_ID, "Time Taken to exit the id repo file. " + trackerRefId + " " + TimeUnit.SECONDS.convert(timeDifference, TimeUnit.NANOSECONDS));
+
+        return responseDto;
+    }
+
+    private Map<String, Object> prepareAdditionalMaps(Map<String, Object> demoDetails, PacketDto packetDto) {
+        DemographicDedupe dedupe = new DemographicDedupe();
+        try {
+            StringBuilder fullName = new StringBuilder();
+            fullName.append((String) demoDetails.get("firstName"))
+                    .append((String) demoDetails.get("lastName"));
+            dedupe.setName(getHMACHashCode(fullName.toString().trim().toUpperCase()));
+            String dob = (String) demoDetails.get("dateOfBirth");
+            dedupe.setDob(getHMACHashCode(dob.replaceAll("-","/")));
+            String gender = (String) demoDetails.get("gender");
+            dedupe.setGender(getHMACHashCode(gender.trim()));
+            if (StringUtils.hasText((String) demoDetails.get("phoneNumber"))) {
+                dedupe.setPhone(getHMACHashCode((String) demoDetails.get("phoneNumber")));
+            }
+            if (StringUtils.hasText((String) demoDetails.get("email"))) {
+                dedupe.setEmail(getHMACHashCode((String) demoDetails.get("email")));
+            }
+            dedupe.setNrcId(getHMACHashCode((String) demoDetails.get("nrcId")));
+            dedupe.setLangCode(ENGLISH_LANG_CODE);
+            dedupe.setIteration(1);
+            dedupe.setRegId(packetDto.getId());
+            dedupe.setProcess(packetDto.getProcess());
+            return Map.of("REF_CODE", demoDetails.get("SUBFOLDER"), "DEMO_DATA", mapper.writeValueAsString(dedupe));
+        } catch (JsonProcessingException e) {
+            logger.error("Error while parsing demo data for additional maps.", e);
+        } catch (NoSuchAlgorithmException e) {
+            logger.error("Error while generating hash for additional maps.", e);
+        }
+        return null;
+    }
+
+    private String getHMACHashCode(String value) throws NoSuchAlgorithmException {
+        if (value == null)
+            return null;
+        return CryptoUtil.encodeToURLSafeBase64(HMACUtils2.generateHash(value.getBytes()));
+    }
+
+    private void trackerStatusUpdate(String refId, PacketDto packetDto, ResultSetter setter, TrackerStatus status, String comment, Map<String, Object> additionalMaps) {
+        ResultDto resultDto = new ResultDto();
+        resultDto.setRegNo(packetDto.getId());
+        resultDto.setRefId(refId);
+        resultDto.setStatus(status);
+        resultDto.setComments(comment);
+        if (additionalMaps != null) {
+            resultDto.setAdditionalMaps(additionalMaps);
+        }
+        setter.setResult(resultDto);
+    }
+
+    private List<Documents> getBiometricsAndDocuments(PacketDto packetDto) {
+        List<Documents> documents = new ArrayList<>();
+        if (packetDto.getBiometrics() != null) {
+            List<BIR> birList = packetDto.getBiometrics().get("individualBiometrics").getSegments();
+            try {
+                byte[] bdbBytes = cbeffUtil.createXML(birList);
+                String encodedCbeffFile = CryptoUtil.encodeToURLSafeBase64(bdbBytes);
+                documents.add(new Documents("individualBiometrics", encodedCbeffFile));
+            } catch (Exception e) {
+                logger.error("Error while creating cbeff file.", e);
+            }
+        }
+        if (packetDto.getDocuments() != null) {
+            if (packetDto.getFields().get("proofOfIdentity") != null) {
+                String encodedDocument = CryptoUtil.encodeToURLSafeBase64(packetDto.getDocuments().get("proofOfIdentity").getDocument());
+                documents.add(new Documents("proofOfIdentity", encodedDocument));
+            }
+        }
+        return documents;
+    }
+
+
+    @PostConstruct
+    public void loadXSD() throws IOException {
+        InputStream inputStream = null;
+        try {
+            inputStream = new ClassPathResource(cbeffXsd).getInputStream();
+            this.xsd = IOUtils.toByteArray(inputStream);
+        } catch (Throwable var5) {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (Throwable var4) {
+                    var5.addSuppressed(var4);
+                }
+            }
+        }
+        if (inputStream != null) {
+            inputStream.close();
+        }
+
+    }
+}
---
+++ data-extractor-ref-impl/src/main/java/io/mosip/packet/data/datapostprocessor/custom/ImportIdentityServiceImpl.java
@@ -0,0 +1,141 @@
+package io.mosip.packet.data.datapostprocessor.custom;
+
+import io.mosip.kernel.core.exception.ExceptionUtils;
+import io.mosip.kernel.core.logger.spi.Logger;
+import io.mosip.packet.core.constant.ApiName;
+import io.mosip.packet.core.constant.LoggerFileConstant;
+import io.mosip.packet.core.dto.ResponseWrapper;
+import io.mosip.packet.core.exception.ApisResourceAccessException;
+import io.mosip.packet.core.logger.DataProcessLogger;
+import io.mosip.packet.core.service.DataRestClientService;
+import io.mosip.packet.data.dto.IdRequestDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class ImportIdentityServiceImpl implements ImportIdentityService {
+
+    private static final String PROVINCE = "province";
+    private static final String DISTRICT = "district";
+    private static final String CONSTITUENCY = "constituency";
+    private static final String SUBFOLDER = "SUBFOLDER";
+    private static final String LANG_CODE = "eng";
+    private static final String HIERARCHY_NAME = "hierarchyName";
+    private static final String NAME = "name";
+
+    private final Logger logger = DataProcessLogger.getLogger(ImportIdentityServiceImpl.class);
+    private Map<String, Map<String, Object>> locationMemoryMap = new ConcurrentHashMap<>();
+
+    @Autowired
+    private DataRestClientService dataRestClientService;
+
+    @Override
+    public ResponseWrapper importIdentity(IdRequestDto idRequestDto, Map<String, Object> demoDetails) throws ApisResourceAccessException {
+        logger.info("Writer Request: {}", idRequestDto);
+        // Fetches location detail
+        getLocationDetails(idRequestDto, (String) demoDetails.get(SUBFOLDER));
+        // Generates UIN
+        generateUIN(idRequestDto);
+        // Add Identity call
+        return addIdentity(idRequestDto);
+    }
+
+    private void getLocationDetails(IdRequestDto idRequestDto, String locationCode) throws ApisResourceAccessException {
+        try {
+            // Location details fetch
+            //String locationCode = (String) ((Map<String, Object>) idRequestDto.getRequest().getIdentity()).get(SUBFOLDER);
+            Map<String, Object> identity = ((Map<String, Object>) idRequestDto.getRequest().getIdentity());
+            if (locationMemoryMap.get(locationCode) != null && !locationMemoryMap.get(locationCode).isEmpty()) {
+                getLocationData(locationMemoryMap.get(locationCode), identity);
+            } else {
+                List<String> pathSegments = Arrays.asList(locationCode, LANG_CODE);
+                ResponseWrapper response = (ResponseWrapper) dataRestClientService.getApi(ApiName.MASTER_LOCATION_GET, pathSegments, "", "", ResponseWrapper.class);
+                if (response != null && response.getResponse() != null) {
+                    Map<String, Object> locationLocalMap = new HashMap<>();
+                    Map<String, Object> responseMap = (Map<String, Object>) response.getResponse();
+                    List<Map<String, Object>> locationDtos = (List<Map<String, Object>>) responseMap.get("locations");
+                    locationDtos.stream().forEach(locationDtoMap -> {
+                            setLocationData(locationDtoMap, identity);
+                            prepareLocationMap(locationLocalMap, locationDtoMap);
+                        }
+                    );
+                    locationMemoryMap.putIfAbsent(locationCode, locationLocalMap);
+                    logger.info("Location fetch success.");
+                }
+            }
+        } catch (ApisResourceAccessException e) {
+            logger.error(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.APPLICATIONID.toString(), LoggerFileConstant.APPLICATIONID.toString(),
+                    e.getMessage() + ExceptionUtils.getStackTrace(e));
+            throw e;
+        }
+    }
+
+    private void getLocationData(Map<String, Object> locationMap, Map<String, Object> identity) {
+        identity.put(PROVINCE, locationMap.get(PROVINCE));
+        identity.put(DISTRICT, locationMap.get(DISTRICT));
+        identity.put(CONSTITUENCY, locationMap.get(CONSTITUENCY));
+    }
+
+    private void setLocationData(Map<String, Object> locationMap, Map<String, Object> identity) {
+        if (PROVINCE.equalsIgnoreCase(String.valueOf(locationMap.get(HIERARCHY_NAME)))) {
+            identity.put(PROVINCE, getLangMap(locationMap));
+        } else if(DISTRICT.equalsIgnoreCase(String.valueOf(locationMap.get(HIERARCHY_NAME)))) {
+            identity.put(DISTRICT, getLangMap(locationMap));
+        } else if(CONSTITUENCY.equalsIgnoreCase(String.valueOf(locationMap.get(HIERARCHY_NAME)))){
+            identity.put(CONSTITUENCY, getLangMap(locationMap));
+        }
+    }
+
+    private void prepareLocationMap(Map<String, Object> locationLocalMap, Map<String, Object> locationDBMap) {
+
+        if (PROVINCE.equalsIgnoreCase(String.valueOf(locationDBMap.get(HIERARCHY_NAME)))) {
+            locationLocalMap.put(PROVINCE, getLangMap(locationDBMap));
+        } else if(DISTRICT.equalsIgnoreCase(String.valueOf(locationDBMap.get(HIERARCHY_NAME)))) {
+            locationLocalMap.put(DISTRICT, getLangMap(locationDBMap));
+        } else if(CONSTITUENCY.equalsIgnoreCase(String.valueOf(locationDBMap.get(HIERARCHY_NAME)))){
+            locationLocalMap.put(CONSTITUENCY, getLangMap(locationDBMap));
+        }
+    }
+
+    private List<Map> getLangMap(Map<String, Object> locationDtoMap) {
+        return Arrays.asList(Map.of("language", LANG_CODE, "value", String.valueOf(locationDtoMap.get(NAME))));
+    }
+
+    private ResponseWrapper addIdentity(IdRequestDto idRequestDto) throws ApisResourceAccessException {
+        //logger.info("Add Identity Request: {}", (new Gson()).toJson(idRequestDto));
+        try {
+
+            ResponseWrapper response = (ResponseWrapper) dataRestClientService.postApi(ApiName.ADD_IDENTITY,
+                    null, "", "", idRequestDto, ResponseWrapper.class);
+            logger.info("Add Identity call completed.");
+            return response;
+        } catch (ApisResourceAccessException e) {
+            logger.error(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.APPLICATIONID.toString(),
+                    LoggerFileConstant.APPLICATIONID.toString(), e.getMessage() + ExceptionUtils.getStackTrace(e));
+            throw e;
+        }
+    }
+
+    private void generateUIN(IdRequestDto idRequestDto) throws ApisResourceAccessException {
+        try {
+            // UIN generation
+            ResponseWrapper response = (ResponseWrapper) dataRestClientService.getApi(ApiName.GET_UIN, null, "", "", ResponseWrapper.class);
+            if (response != null && response.getResponse() != null) {
+                Map<String, String> responseMap = (Map<String, String>) response.getResponse();
+                ((Map<String, Object>) idRequestDto.getRequest().getIdentity()).put("UIN", responseMap.get("uin"));
+                logger.info("UIN Generation success.");
+            }
+        } catch (ApisResourceAccessException e) {
+            logger.error(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.APPLICATIONID.toString(), LoggerFileConstant.APPLICATIONID.toString(),
+                    e.getMessage() + ExceptionUtils.getStackTrace(e));
+            throw e;
+        }
+    }
+
+}
